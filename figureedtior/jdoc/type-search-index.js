b070dbd4a6f059144a234b484b7c9ccbefb17d08
Aarshee Mishra <aarsheemishr@umass.edu>
2020-10-24 20:54:23 -0400

    HW1

---
+++ figureedtior/jdoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"controller","l":"FigureController"},{"p":"<Unnamed>","l":"FigureEditor"},{"p":"model","l":"FigureModel"},{"p":"view","l":"FigureView"},{"p":"view","l":"InfoView"},{"p":"view","l":"View"}]
